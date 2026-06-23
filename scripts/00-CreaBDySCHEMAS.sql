220d5e1d4864b72e0520267a30f771afeafb98df
FacuGa <guardiaimperial06@gmail.com>
2026-06-23 20:39:34 -0300

    DER Aprobado, DOC TECNICO APROBADO, empezamos a enumerar los .sql

---
+++ scripts/00-CreaBDySCHEMAS.sql
@@ -0,0 +1,27 @@
+/*******************************************************************************
+                       UNIVERSIDAD NACIONAL DE LA MATANZA 
+Integrantes: [Nicolás Kugel, Facundo Gargiulo, Valentin Martinez]
+Descripción:  Creacion de Base de datos y Schemas
+*******************************************************************************/
+USE master
+GO
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'TPBDG5')
+BEGIN
+    DROP DATABASE TPBDG5;
+END
+GO
+    CREATE DATABASE TPBDG5;
+GO
+
+USE TPBDG5;
+
+GO
+
+CREATE SCHEMA Personal;
+GO
+CREATE SCHEMA Concesiones;
+GO              
+CREATE SCHEMA Parques;
+GO
+CREATE SCHEMA Ventas;
+GO
